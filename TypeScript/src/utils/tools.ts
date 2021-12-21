4776c8812a8267cf803efb26fba1c5f590128bf3
katai5plate <katai5plate.sub@gmail.com>
2021-12-21 23:17:18 +0900

    add listToArray & refacoring config

--- TypeScript/src/utils/tools.ts
+++ TypeScript/src/utils/tools.ts
@@ -3,3 +3,7 @@ import { System } from "csharp";
 /** System.Array 型を JS で扱いやすい配列にする */
 export const arrayToArray = <T>(array: System.Array): T[] =>
   [...new Array(array.GetLength(0)).keys()].map((i) => array.GetValue(i));
+
+/** List 型を JS で扱いやすい配列にする */
+export const listToArray = <T>(list: System.Collections.Generic.List$1<T>) =>
+  [...new Array(list.Count).keys()].map((i) => list.get_Item(i));
